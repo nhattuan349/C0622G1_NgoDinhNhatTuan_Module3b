50a0da0d8ee912959ed73c93683053ea31c3362e
unknown <nhattuanqlmt2@gmail.com>
2022-10-03 20:56:44 +0700

    update

---
+++ ss12_jdbc_va_crud/exercise/jdbc_crud/src/main/java/service/impl/UserService.java
@@ -0,0 +1,42 @@
+package service.impl;
+
+import model.User;
+import repository.IUserDAO;
+import repository.impl.UserDAO;
+import service.IUserService;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserService implements IUserService {
+    private IUserDAO userDAO = new UserDAO();
+    @Override
+    public void insertUser(User user) throws SQLException {
+        userDAO.insertUser(user);
+    }
+
+    @Override
+    public User selectUser(int id) {
+        return userDAO.selectUser(id);
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return userDAO.selectAllUsers();
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        return userDAO.deleteUser(id);
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        return userDAO.updateUser(user);
+    }
+
+    @Override
+    public List<User> findByName(String userName) {
+        return userDAO.findByName(userName);
+    }
+}
