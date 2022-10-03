849af6ffc6717b936ffbcd41eefa91b2e335e33b
unknown <nhattuanqlmt2@gmail.com>
2022-10-03 14:02:42 +0700

    update

---
+++ ss12_jdbc_va_crud/exercise/jdbc_and_crud/src/main/java/config/FilterUTF8.java
@@ -0,0 +1,14 @@
+package config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "FilterUTF-8", value = "/*")
+public class FilterUTF8 implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+}
