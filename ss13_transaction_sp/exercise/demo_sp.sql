20e23b63cf1c3cdd35b724f47f601ea1573144e2
unknown <nhattuanqlmt2@gmail.com>
2022-10-04 16:15:24 +0700

    update

--- ss13_transaction_sp/exercise/demo_sp.sql
+++ ss13_transaction_sp/exercise/demo_sp.sql
@@ -12,3 +12,38 @@ create table users (
 insert into users(name, email, country) values('Minh','minh@codegym.vn','Viet Nam');
 insert into users(name, email, country) values('Kante','kante@che.uk','Kenia');
 
+--Gọi Stored Procedures từ JDBC sử dụng CallableStatement cho chức năng hiển thị danh sách users
+Delimiter $$
+create procedure show_list()
+begin
+	select * from users;
+end $$
+Delimiter ;
+
+call show_list();
+
+-- Gọi Stored Procedures từ JDBC sử dụng CallableStatement cho chức năng sửa thông tin user
+Delimiter $$
+create procedure edit_sp(id_sp int, name_sp varchar(45), email_sp varchar(45), country_sp varchar(45))
+begin
+	update users 
+    set name = name_sp,
+				email= email_sp,
+				country =country_sp 
+	where id = id_sp;
+end $$
+Delimiter ;
+
+call edit_sp();
+
+-- Gọi Stored Procedures từ JDBC sử dụng CallableStatement cho chức năng xoá user
+Delimiter $$
+create procedure delete_sp(id_sp int)
+begin
+	delete from users where id = id_sp;
+end $$
+Delimiter ;
+
+call delete_sp(2);
+
+select * from users;
